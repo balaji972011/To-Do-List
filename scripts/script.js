e19f5f1218fc03e68fdde96867406979ef50dc82
balaji972011 <120183446+balaji972011@users.noreply.github.com>
2023-02-24 23:57:29 +0530

    add edit & update function

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,15 @@ const enterListEl = document.getElementById(`enter-list`);
 const itemEl = document.getElementById(`item`);
 
 let items = [];
+let editId = null;
+let editing = false;
 
 const displayItems = function () {
   itemEl.innerHTML = null;
   items.forEach((item) => {
     const listEl = document.createElement(`li`);
     listEl.classList.add(`item-list`);
-    listEl.innerHTML = `${item.value} <button onclick='deleteItem(${item.key})'><i class="fa-solid fa-trash-can"></i></button>`;
+    listEl.innerHTML = `${item.value} <button onclick='editItem(${item.key})'><i class="fa-regular fa-pen-to-square"></i></i></button> <button onclick='deleteItem(${item.key})'><i class="fa-solid fa-trash-can"></i></button>`;
     itemEl.appendChild(listEl);
   });
 };
@@ -20,13 +22,35 @@ const deleteItem = function (key) {
   displayItems();
 };
 
+const editItem = function (key) {
+  const itemToEdit = items.find((item) => item.key === key);
+  enterListEl.value = itemToEdit.value;
+  editId = key;
+  editing = true;
+  displayItems();
+};
+
 formEl.addEventListener(`submit`, (e) => {
   e.preventDefault();
   const item = { key: new Date().valueOf(), value: enterListEl.value };
   if (enterListEl.value) {
-    items.push(item);
-    enterListEl.value = null;
-    displayItems();
+    if (editing) {
+      items = items.map((item) => {
+        if (item.key === editId) {
+          return { ...item, value: enterListEl.value };
+        } else {
+          return item;
+        }
+      });
+      displayItems();
+      editId = null;
+      editing = false;
+      enterListEl.value = null;
+    } else {
+      items.push(item);
+      displayItems();
+      enterListEl.value = null;
+    }
   } else {
     alert(`Enter Valid Input`);
   }
